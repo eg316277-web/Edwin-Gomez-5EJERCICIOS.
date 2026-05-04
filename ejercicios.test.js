a6cb365a9d86536f23bed9526e63fa51e09b82d8
Edwin Gomez <eg316277@gmail.com>
2026-05-04 12:41:09 -0500

    Sistema completo de testing y badge visual

--- ejercicios.test.js
+++ ejercicios.test.js
@@ -4,10 +4,16 @@ import { criba } from './ejemplo28.js';
 import { matrixMultiplication } from './ejemplo29.js';
 import { mergeSort } from './ejemplo30.js';
 
-test('Pruebas Ejercicios 26-30', () => {
+test('Suite de Pruebas Ejercicios 26-30', () => {
+    // Ejercicio 26
     expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]).maxSum).toBe(6);
-    expect(mergeSortedArrays([1,3], [2,4])).toEqual([1,2,3,4]);
+    // Ejercicio 27
+    expect(mergeSortedArrays([1,3,5], [2,4,6])).toEqual([1,2,3,4,5,6]);
+    // Ejercicio 28
     expect(criba(10)).toEqual([2,3,5,7]);
-    expect(matrixMultiplication([[1,2],[3,4]], [[5,6],[7,8]])).toEqual([[19,22],[43,50]]);
+    // Ejercicio 29
+    const m1 = [[1,2],[3,4]], m2 = [[5,6],[7,8]];
+    expect(matrixMultiplication(m1, m2)).toEqual([[19,22],[43,50]]);
+    // Ejercicio 30
     expect(mergeSort([5,3,8,1,2])).toEqual([1,2,3,5,8]);
 });
